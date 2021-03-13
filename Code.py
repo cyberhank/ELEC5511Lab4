ac0becff151a8ab2b8d02bc18e406f8b25e9252f
Henry Benerman <cyberhank@comcast.net>
2021-03-13 15:52:20 -0700

    Mostly runs except for the fc_forward function

--- Code.py
+++ Code.py
@@ -1,10 +1,15 @@
 import gzip,pickle,sys
 import matplotlib.pyplot as plt
 import numpy as np
+from  Network import NeuralNetwork
+import pandas as pd
 
-conv1_w = np.load("conv1_w.np")
+convy = NeuralNetwork()
+
+conv1_w = np.load("conv1_w.npy")
 conv1_b = np.load("conv1_b.npy")
 conv2_w = np.load("conv2_w.npy")
+conv2_b = np.load("conv2_b.npy")
 fc_w = np.load("fc_w.npy")
 fc_b = np.load("fc_b.npy")
 
@@ -14,11 +19,21 @@ if sys.version_info < (3,):
 else:
     (X_train, y_train), (X_test, y_test) = pickle.load(f, encoding="bytes")
 
+
 idx=np.random.choice(X_test.shape[0],5)
 x,y=X_test[idx],y_test[idx]
 x = np.reshape(x,(-1,1,28,28))
+conv1 = convy.conv_forward(x.astype(np.float64),conv1_w,conv1_b, 1)
+conv1_relu = convy.relu_forward(conv1)
+maxp1 = convy.max_pooling_forward(conv1_relu.astype(np.float64),2)
 
-
+conv2 = convy.conv_forward(maxp1,conv2_w,conv2_b, 1)
+conv2_relu = convy.relu_forward(conv2)
+maxp2 = convy.max_pooling_forward(conv2_relu,1)
+flatten = convy.flatten_forward(maxp2)
+print(np.shape(fc_w))
+print(np.shape(fc_b))
+y = convy.fc_forward(flatten,fc_w,fc_b)
 for i in range(5):
     plt.figure(figsize=(3,3))
     plt.imshow(np.reshape(x[i],(28,28)), cmap='gray')
--- Network.py
+++ Network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.signal as sps
 class NeuralNetwork():
     def __init__(self):
         return
@@ -11,20 +12,33 @@ class NeuralNetwork():
         return np.dot(W, z.transpose()) + np.expand_dims(b,1).repeat(z.shape[0],axis = 1)
     def conv_forward(self, z, K, b):
         return conv_z
-    def max_pooling_forward(self,z):
-        return pool_z
-    def padding(self,x, padding):
-        y = []
-        for i in range(np.size(x, axis=1)+2):
-           for j in range(np.size(x, axis=0)+2):
-               if i == 0:
-                   y[i,j] = padding
-               if i == np.size(x, axis=1)+2:
-                    y[i,j] = padding
-               if j == 0:
-                   y[i,j] = padding
-               if i == np.size(x, axis=0)+2:
-                    y[i,j] = padding
+    def max_pooling_forward(self,z,stride):
+         (N,C,H,W) = z.shape
+         h = int(H/stride)
+         w = int(W/stride)
+            
+         pool_z = np.zeros([N,C,h,w])
+         for n in range (N):
+             for c in range (C):
+                 for d in range (h):
+                     for e in range (w):
+                            pool_z[n,c,d,e] = np.max(z[n,c,d*stride:d*stride+2,e*stride:e*stride+2])
+                          
+                     return pool_z
+    def conv_forward(self,z,K,b,padding):
+        padding_z = np.lib.pad(z, ((0, 0), (0, 0), (padding, padding), (padding, padding)), 'constant', constant_values=0)
+        size = np.array(np.shape(padding_z))
+        size.flatten()
+        (N,C,H,W) = z.shape
+        y = np.zeros([N,C,H,W])
+       
+        for i in range(size[0]):
+            for j in range(size[1]):
+                for k in range(1,size[2]-2):                 
+                    for l in range(1,size[3]-2):
+                        y[i,j,k,l] += sum(sum(padding_z[i,j,(k-1):(k+2),(l-1):(l+2)]*K[j,0,:,:]))
+       
+        y = np.array(y)     
 
         return y
     pass
