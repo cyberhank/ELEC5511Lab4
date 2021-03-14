1bb892d416750adb493e001e9aa28517cef122f5
Henry Benerman <cyberhank@comcast.net>
2021-03-14 14:15:41 -0600

    Working Code

--- Code.py
+++ Code.py
@@ -4,36 +4,33 @@ import numpy as np
 from  Network import NeuralNetwork
 import pandas as pd
 
-convy = NeuralNetwork()
+convy = NeuralNetwork() # enstantiates NeuralNetwork object
 
-conv1_w = np.load("conv1_w.npy")
+# Loads files
+conv1_w = np.load("conv1_w.npy") 
 conv1_b = np.load("conv1_b.npy")
 conv2_w = np.load("conv2_w.npy")
 conv2_b = np.load("conv2_b.npy")
 fc_w = np.load("fc_w.npy")
 fc_b = np.load("fc_b.npy")
 
+# Unpickleing of initial files
 f = gzip.open('mnist.pkl.gz','rb')
 if sys.version_info < (3,):
     (X_train, y_train),(X_test, y_test) = pickle.load(f)
 else:
     (X_train, y_train), (X_test, y_test) = pickle.load(f, encoding="bytes")
 
-
+#Random pick of five starter files
 idx=np.random.choice(X_test.shape[0],5)
 x,y=X_test[idx],y_test[idx]
 x = np.reshape(x,(-1,1,28,28))
-conv1 = convy.conv_forward(x.astype(np.float64),conv1_w,conv1_b, 1)
-conv1_relu = convy.relu_forward(conv1)
-maxp1 = convy.max_pooling_forward(conv1_relu.astype(np.float64),2)
 
-conv2 = convy.conv_forward(maxp1,conv2_w,conv2_b, 1)
-conv2_relu = convy.relu_forward(conv2)
-maxp2 = convy.max_pooling_forward(conv2_relu,2)
-flatten = convy.flatten_forward(maxp2)
-
-y = convy.fc_forward(flatten,fc_w,fc_b)
+#running of the code
+y = convy.forwrd(x,conv1_w,conv1_b,conv2_w,conv2_b,fc_w,fc_b)
 print(y)
+
+#Displaying of the results
 for i in range(5):
     plt.figure(figsize=(3,3))
     plt.imshow(np.reshape(x[i],(28,28)), cmap='gray')
--- Network.py
+++ Network.py
@@ -11,38 +11,53 @@ class NeuralNetwork():
     def fc_forward(self, z, W, b):
         return np.dot(W, z.transpose()) + np.expand_dims(b,1).repeat(z.shape[0],axis = 1)
     
-    def max_pooling_forward(self,z,stride):
-         (N,C,H,W) = z.shape
-         h = int(H/stride)
-         w = int(W/stride)
+    def max_pooling_forward(self,z,stride =2 ):
+        (N,C,H,W) = z.shape # creating variables for the size of the input matrix
+        h = int(H/stride) # scaling of the height
+        w = int(W/stride) # scalling the width
             
-         pool_z = np.zeros([N,C,h,w])
-         for n in range (N):
-             for c in range (C):
-                 for d in range (h):
-                     for e in range (w):
-                            pool_z[n,c,d,e] = np.max(z[n,c,d*stride:d*stride+2,e*stride:e*stride+2])
-                          
-                     return pool_z
-    def conv_forward(self,z,K,b,padding):
-        padding_z = np.lib.pad(z, ((0, 0), (0, 0), (padding, padding), (padding, padding)), 'constant', constant_values=0)
-        size = np.array(np.shape(padding_z))
-        size.flatten()
-        (N,C,H,W) = z.shape
-       # y = np.zeros([N,C,H,W])
-        D, C, k1, k2 = K.shape
-        y = np.zeros([N,D,H,W])
+        pool_z = np.zeros([N,C,h,w])
+        for n in range (N): # iterates throught the input files
+            for c in range (C): # iterates through the channels
+                # the last two for-loops iterate through the convolved and relu corrected functions
+                for d in range (h): 
+                    for e in range (w):
+                        pool_z[n,c,d,e] = np.max(z[n,c,d*stride:d*stride+2,e*stride:e*stride+2]) # computes the max pooling of the correct slices
+        return pool_z   
+    def conv_forward(self, z, K, b, padding=0):
        
-        for i in range(size[0]):
-            for j in range(D):
-                for k in range(size[2]-2):                 
-                    for l in range(size[3]-2):
-                        #y[i,j,k,l] += sum(sum(padding_z[i,j,(k-1):(k+2),(l-1):(l+2)]*K[j,0,:,:]))
-                        y[i,j,k,l] =  np.sum(padding_z[i, :, k:k + k1, l:l + k2] * K[j, :]) + b[j]
-       
-            
+       stride = 1 # forcing stride to be 1
+       padding_z = np.lib.pad(z, ((0, 0), (0, 0), (padding, padding), (padding, padding)), 'constant', constant_values=0) # line that automatically pads the function
+       (N,C,H,W) = padding_z.shape # the following gathers the dimensions for the for loops
+       (D,C,k1,k2) = K.shape 
+       (OC,OD,OH,OW) = z.shape
+
+       oH = int((OH+2*padding-k1)/stride)+1 # Scalling of the height
+       oW = int((OW+2*padding-k2)/stride)+1 # scalling of the width for the ooutput matix
+
+       conv_z = np.zeros([N,D,oH,oW]) # since appending numpy arrays are not computationally efficient, this makes an array of zeros of the right size that can be updated later
+       for i in range (N): # iterates through the input images
+          for j in range (D): # iterates through the filters
+              # the rest goes through the height and width of the input images
+              for h in range (oH): 
+                  for w in range (oW):
+                      conv_z[i,j,h,w] = np.sum(K[j,:] *padding_z[i,:,h:h+k1,w:w+k2])+b[j] # this code essentially takes the dot product of the input matrix section and the filter
+       return conv_z
+    def forwrd(self,x,conv1_w,conv1_b,conv2_w,conv2_b,fc_w,fc_b):
+
+        conv1       =   self.conv_forward(x.astype(np.float64),conv1_w,conv1_b,padding=1)
+        conv1_relu  =   self.relu_forward(conv1)
+        maxp1       =   self.max_pooling_forward(conv1_relu.astype(np.float64))
+
+        conv2       =   self.conv_forward(maxp1, conv2_w, conv2_b, padding=1)
+        conv2_relu  =   self.relu_forward(conv2)
+        maxp2       =   self.max_pooling_forward(conv2_relu.astype(np.float64))
+
+        flatten     =   self.flatten_forward(maxp2)
+        y           =   self.fc_forward(flatten,fc_w,fc_b)
 
         return y
+                       
     pass
 
 
